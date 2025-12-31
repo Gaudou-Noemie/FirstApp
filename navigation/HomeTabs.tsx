880a8ee2a35bf6bd862f953be6c50737adb9fd49
Noémie Gaudou <gaudou.noemie@gmail.com>
2025-12-31 19:40:25 +0100

    modification

--- navigation/HomeTabs.tsx
+++ navigation/HomeTabs.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-import BookSearch from '../screens/BookSearch';
 import MyBooks from '../screens/MyBooks';
 
 const Tab = createBottomTabNavigator();
@@ -9,12 +7,6 @@ const Tab = createBottomTabNavigator();
 export default function HomeTabs() {
   return (
     <Tab.Navigator>
-
-      <Tab.Screen
-        name="Search"
-        component={BookSearch}
-        options={{ title: 'Recherche' }}
-      />
 
       <Tab.Screen
         name="WishBooks"
